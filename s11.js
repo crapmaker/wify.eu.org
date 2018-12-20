58a1832b5743095bb18299f26c1527f6c455d10f
crapmaker <46024265+crapmaker@users.noreply.github.com>
2018-12-20 20:40:49 +0530

    Create s11.js

---
+++ s11.js
@@ -0,0 +1,28 @@
+let container = document.getElementById("cnt");
+let content = document.createElement("div");
+content.innerHTML = `<h1 class="has-text-centered">All the Difference in The World </h1>
+<br/>
+<p>Every Sunday morning I take a light jog around a park near my home. There’s a lake
+    located in one corner of the park. Each time I jog by this lake, I see the same elderly woman sitting at the water’s
+    edge with a small metal cage sitting beside her. </p>
+<p>This past Sunday my curiosity got the best of me, so I stopped jogging
+    and walked over to her. As I got closer, I realized that the metal cage was in fact a small trap. There were three
+    turtles, unharmed, slowly walking around the base of the trap. </p>
+<p>She had a fourth turtle in her lap that she was carefully scrubbing with a spongy brush. “Hello,” I said. “I see you here every Sunday morning. If you don’t mind my
+    nosiness, I’d love to know what you’re doing with these turtles.”</p>
+<p>She smiled. “I’m cleaning off their shells,” she replied. “Anything on a turtle’s shell, like algae or scum, reduces
+    the turtle’s ability to absorb heat and impedes its ability to swim. It can also corrode and weaken the shell over
+    time.” “Wow! That’s really nice of you!” I exclaimed.</p>
+<p>She went on: “I spend a couple of hours each Sunday morning, relaxing by this lake and helping these little guys out.
+    It’s my own strange way of making a difference.”“But don’t most freshwater turtles live their whole lives with algae 
+    and scum hanging from their shells?” I asked.</p>
+<p>“Yep, sadly, they do,” she replied. I scratched my head. “Well then, don’t you think your time could be better spent? I
+    mean, I think your efforts are kind and all, but there are fresh water turtles living in lakes all around the world.
+    And 99% of these turtles don’t have kind people like you to help them clean off their shells. So, no offense… but how
+    exactly are your localized efforts here truly making a difference?” The woman giggled aloud. </p>
+<p>She then looked down at the turtle in her lap, scrubbed off the last piece of algae from its shell, and said, “Sweetie, if this little guy
+    could talk, he’d tell you I just made all the difference in the world.” The moral: You can change the world – maybe not
+    all at once, but one person, one animal, and one good deed at a time. Wake up every morning and pretend like what you
+    do makes a difference. It does.</p>`
+container.appendChild(content); 
+ 
