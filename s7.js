41f8ff80cf78b5a8b425a0c9b1ea94eb2f3cd8ae
crapmaker <46024265+crapmaker@users.noreply.github.com>
2018-12-20 19:29:10 +0530

    Create s7.js

---
+++ s7.js
@@ -0,0 +1,18 @@
+let container = document.getElementById("cnt");
+let content = document.createElement("div");
+content.innerHTML = `<h1 class="has-text-centered"> You are special</h1>
+<br />
+<p>Value A popular speaker started off a seminar by holding up a $20 bill. A crowd of 200 had gathered to hear him speak.
+    He asked, “Who would like this $20 bill?”</p>
+<p>200 hands went up. He said, “I am going to give this $20 to one of you but first, let me do this.” He crumpled the bill
+    up.He then asked, “Who still wants it?”</p>
+<p>All 200 hands were still raised. “Well,” he replied, “What if I do this?” Then he dropped the bill on the ground and
+    stomped on it with his shoes.</p>
+<p>He picked it up, and showed it to the crowd. The bill was all crumpled and dirty.
+    “Now who still wants it?” All the hands still went up.</p>
+<p>“My friends, I have just showed you a very important lesson. No matter what I did to the money, you still wanted it
+    because it did not decrease in value. It was still worth $20. Many times in our lives, life crumples us and grinds us
+    into the dirt.</p>
+<p>We make bad decisions or deal with poor circumstances. We feel worthless. But no matter what has
+    happened or what will happen, you will never lose your value. You are special – Don’t ever forget it!</p>`
+container.appendChild(content); 
