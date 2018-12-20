bf4fc366a25c74f0888efd3de6daed82825af549
crapmaker <46024265+crapmaker@users.noreply.github.com>
2018-12-20 19:25:59 +0530

    Create s6.js

---
+++ s6.js
@@ -0,0 +1,16 @@
+let container = document.getElementById("cnt");
+let content = document.createElement("div");
+content.innerHTML = `<h1 class="has-text-centered">The Obstacle in our Path</h1>
+<br />
+<p>The Obstacle in our Path There once was a very wealthy and curious king. This king had a huge boulder placed in the
+    middle of a road. Then he hid nearby to see if anyone would try to remove the gigantic rock from the road. </p>
+<p>The first people to pass by were some of the king’s wealthiest merchants and courtiers. Rather than moving it, they simply walked
+    around it. A few loudly blamed the King for not maintaining the roads.</p>
+<p>Not one of them tried to move the boulder.Finally, a peasant came along. His arms were full of vegetables. When he got near the boulder, rather than simply
+    walking around it as the others had, the peasant put down his load and tried to move the stone to the side of the road.
+    It took a lot of effort but he finally succeeded.</p>
+<p>The peasant gathered up his load and was ready to go on his way when he say a purse lying in the road where the boulder
+    had been. The peasant opened the purse. The purse was stuffed full of gold coins and a note from the king. The king’s
+    note said the purse’s gold was a reward for moving the boulder from the road. The king showed the peasant what many of
+    us never understand: every obstacle presents an opportunity to improve our condition.</p>`
+container.appendChild(content); 
