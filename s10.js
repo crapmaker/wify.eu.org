f7ec9e902e895efa6c52a53be26a03b120ccf9f8
crapmaker <46024265+crapmaker@users.noreply.github.com>
2018-12-20 20:35:05 +0530

    Create s10.js

---
+++ s10.js
@@ -0,0 +1,21 @@
+let container = document.getElementById("cnt");
+let content = document.createElement("div");
+content.innerHTML = `<h1 class="has-text-centered">The Elephant Rope</h1>
+<br />
+<p>The Weight of the Glass Once upon a time a psychology professor walked around on a stage while teaching stress
+    management principles to an auditorium filled with students. As she raised a glass of water, everyone expected they’d
+    be asked the typical “glass half empty or glass half full” question.</p>
+<p>Instead, with a smile on her face, the professor
+    asked, “How heavy is this glass of water I’m holding?” Students shouted out answers ranging from eight ounces to a
+    couple pounds.</p>
+<p>She replied, “From my perspective, the absolute weight of this glass doesn’t matter. It all depends on how long I hold
+    it. If I hold it for a minute or two, it’s fairly light. If I hold it for an hour straight, its weight might make my
+    arm ache a little.</p>
+<p>If I hold it for a day straight, my arm will likely cramp up and feel completely numb and paralyzed,
+    forcing me to drop the glass to the floor. In each case, the weight of the glass doesn’t change, but the longer I hold
+    it, the heavier it feels to me.” As the class shook their heads in agreement. </p>
+<p>she continued, “Your stresses and worries
+    in life are very much like this glass of water. Think about them for a while and nothing happens. Think about them a
+    bit longer and you begin to ache a little. Think about them all day long, and you will feel completely numb and
+    paralyzed – incapable of doing anything else until you drop them.”</p>`
+container.appendChild(content); 
