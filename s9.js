eba12ea826d48924aba795426f2e52993b3648ed
crapmaker <46024265+crapmaker@users.noreply.github.com>
2018-12-20 20:32:26 +0530

    Create s9.js

---
+++ s9.js
@@ -0,0 +1,16 @@
+let container = document.getElementById("cnt");
+let content = document.createElement("div");
+content.innerHTML = `<h1 class="has-text-centered">The Blind Girl</h1>
+<br />
+<p>There was a blind girl who hated herself purely for the fact she was blind. The only person she didn’t hate was her
+    loving boyfriend, as he was always there for her. She said that if she could only see the world, she would marry him.</p>
+<p>One day, someone donated a pair of eyes to her – now she could see everything, including her boyfriend. Her boyfriend
+    asked her, “now that you can see the world, will you marry me?” </p>
+<p>The girl was shocked when she saw that her boyfriendwas blind too, and refused to marry him. Her boyfriend walked away 
+    in tears, and later wrote a letter to her saying:
+    “Just take care of my eyes dear.”</p>
+<p>Moral of the story: When our circumstances change, so does our mind. Some people may not be able to see the way things
+    were before, and might not be able to appreciate them. There are many things to take away from this story, not just
+    one.</p>
+<p>This is one of the inspirational short stories that left me speechless.</p>`
+container.appendChild(content); 
