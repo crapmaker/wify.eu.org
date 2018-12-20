191cfb5cc4f074e64be442ebcf94c32b55ab3099
crapmaker <46024265+crapmaker@users.noreply.github.com>
2018-12-20 20:47:20 +0530

    Create s13.js

---
+++ s13.js
@@ -0,0 +1,17 @@
+let container = document.getElementById("cnt");
+let content = document.createElement("div");
+content.innerHTML = `<h1 class="has-text-centered">Being and Breathing </h1>
+<br />
+<p>One warm evening many years ago…</p>
+<p>After spending nearly every waking minute with Angel for eight straight days, I knew that I had to tell her just one
+        thing. So late at night, just before she fell asleep, I whispered it in her ear. </p>
+<p>She smiled – the kind of smile that makes me smile back –and she said, “When I’m seventy-five and I think about my life and what it was like to 
+        be young, I hope that I can remember this very moment.” A few seconds later she closed her eyes and fell asleep. The room was
+        peaceful – almost silent. All I could hear was the soft purr of her breathing. </p>
+<p>I stayed awake thinking about the time
+        we’d spent together and all the choices in our lives that made this moment possible. And at some point, I realized that
+        it didn’t matter what we’d done or where we’d gone. Nor did the future hold any significance. </p>
+<p>All that mattered was the
+        serenity of the moment.
+        Just being with her and breathing with her.</p>`
+container.appendChild(content); 
