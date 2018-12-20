34937dfaad75e6e4c9be0120961080fdb7828ced
crapmaker <46024265+crapmaker@users.noreply.github.com>
2018-12-20 19:11:27 +0530

    Create s3.js

---
+++ s3.js
@@ -0,0 +1,11 @@
+let container = document.getElementById("cnt");
+let content = document.createElement("div");
+content.innerHTML = `<h1 class="has-text-centered">Shake off Your Problems</h1>
+<br/>
+<p> A man’s favorite donkey falls into a deep precipice. He can’t pull it out no matter how hard he tries. He therefore
+    decides to bury it alive. Soil is poured onto the donkey from above. </p>
+<p>The donkey feels the load, shakes it off, and steps on it. More soil is poured. It shakes it off and steps up. The more the load was poured, the higher it rose. By
+    noon, the donkey was grazing in green pastures. </p>
+<p>After much shaking off (of problems) And stepping up (learning from
+    them), One will graze in GREEN PASTURES.</p>`
+container.appendChild(content); 
