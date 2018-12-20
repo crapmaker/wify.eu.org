e417cd2f560e97ebc862f95850ea542d53eca144
crapmaker <46024265+crapmaker@users.noreply.github.com>
2018-12-20 20:29:02 +0530

    Create s8.js

---
+++ s8.js
@@ -0,0 +1,17 @@
+let container = document.getElementById("cnt");
+let content = document.createElement("div");
+content.innerHTML = `<h1 class="has-text-centered">right place at the right time</h1>
+<br />
+<p>A mother and a baby camel were lying around under a tree.
+    Then the baby camel asked, “Why do camels have humps?”</p>
+<p>The mother camel considered this and said, “We are desert animals so we have the humps to store water so we can survive
+    with very little water.” The baby camel thought for a moment then said, “Ok…why are our legs long and our feet
+    rounded?”</p>
+<p>The mama replied, “They are meant for walking in the desert.”
+    The baby paused. After a beat, the camel asked, “Why are our eyelashes long? Sometimes they get in my way.” The mama
+    responded, “Those long thick eyelashes protect your eyes from the desert sand when it blows in the wind.</p>
+<p>The baby thought and thought. Then he said, “I see. So the hump is to store water when we are in the desert, the legs
+    are for walking through the desert and these eye lashes protect my eyes from the desert then why in the Zoo?” </p>
+<p>The Lesson: Skills and abilities are only useful if you are in the right place at the right time. Otherwise they go to
+    waste.</p>`
+container.appendChild(content); 
