413591ca58dd068ee78435b965035f08a5204b67
crapmaker <46024265+crapmaker@users.noreply.github.com>
2018-12-20 19:22:26 +0530

    Create s5.js

---
+++ s5.js
@@ -0,0 +1,15 @@
+let container = document.getElementById("cnt");
+let content = document.createElement("div");
+content.innerHTML = `<h1 class="has-text-centered">always believe in yourself</h1>
+<br />
+<p>Colonel Sanders | Kentucky Fried Chicken Once, there was an older man, who was broke, living in a tiny house and owned
+    a beat up car. He was living off of $99 social security checks. At 65 years of age, he decide things had to change. </p>
+<p>So he thought about what he had to offer. His friends raved about his chicken recipe. He decided that this was his best
+    shot at making a change. He left Kentucky and traveled to different states to try to sell his recipe. </p>
+<p>He told restaurant owners that he had a mouthwatering chicken recipe. He offered the recipe to them for free, just asking for a
+    small percentage on the items sold. Sounds like a good deal, right? Unfortunately, not to most of the restaurants. </p>
+<p>He heard NO over 1000 times. Even after all of those rejections, he didn’t give up. He believed his chicken recipe was
+    something special. He got rejected 1009 times before he heard his first yes. With that one success Colonel Hartland
+    Sanders changed the way Americans eat chicken. Kentucky Fried Chicken, popularly known as KFC, was born.</p>
+<p>Remember, never give up and always believe in yourself in spite of rejection.</p>`
+container.appendChild(content); 
